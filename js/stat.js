d965761b9290aef26b0e9505b758e394f98a500f
M1nardi <popov.frontend@gmail.com>
2018-04-05 21:17:19 +0300

    Исправил недочеты

--- js/stat.js
+++ js/stat.js
@@ -10,7 +10,9 @@ var BAR_HEIGHT = -150;
 var BAR_X = 160;
 var BAR_Y = 230;
 var BAR_GAP = 50;
-
+var TEXT_FONT = '16px PT Mono';
+var TEXT_COLOR = '#000';
+var USER_COLUMN_COLOR = 'rgba(255, 0, 0, 1)';
 
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
@@ -33,25 +35,43 @@ window.renderStatistics = function (ctx, players, times) {
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
 
-  var maxTime = getMaxElement(times);
+  function defineFontStyle(fontFamily, textColor, baseline) {
+    ctx.font = fontFamily;
+    ctx.fillStyle = textColor;
+    ctx.textBaseline = baseline;
+  }
 
-  for (var i = 0; i < players.length; i++) {
+  function showText(text, x, y) {
+    ctx.fillText(text, x, y);
+  }
 
+  function getRandomColor() {
     if (players[i] === 'Вы') {
-      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+      ctx.fillStyle = USER_COLUMN_COLOR;
     } else {
       ctx.fillStyle = 'rgb(0,' + Math.floor(255 - 60.5 * i) + ',' + Math.floor(255 - 23.5 * i) + ')';
     }
-
-    ctx.fillRect(BAR_X + (BAR_WIDTH + BAR_GAP) * i, BAR_Y, BAR_WIDTH, (BAR_HEIGHT * times[i]) / maxTime);
-    ctx.fillStyle = '#000';
-    ctx.fillText(players[i], BAR_X + (BAR_WIDTH + BAR_GAP) * i, 250);
-    ctx.fillText(Math.round(times[i]), BAR_X + (BAR_WIDTH + BAR_GAP) * i, 30);
   }
 
-  ctx.font = '16px PT Mono';
-  ctx.fillStyle = '#000';
-  ctx.textBaseline = 'hanging';
-  ctx.fillText('Ура вы победили!', 130, 30);
-  ctx.fillText('Список результатов:', 130, 50);
+  function renderPlayersColumns(x, y, width, height) {
+    ctx.fillRect(x, y, width, height);
+  }
+
+  var maxTime = getMaxElement(times);
+
+  for (var i = 0; i < players.length; i++) {
+    var barSpace = BAR_X + (BAR_WIDTH + BAR_GAP) * i;
+    var dynamicBarHeight = (BAR_HEIGHT * times[i]) / maxTime;
+
+    getRandomColor();
+    renderPlayersColumns(barSpace, BAR_Y, BAR_WIDTH, dynamicBarHeight);
+
+    ctx.fillStyle = TEXT_COLOR;
+    ctx.fillText(players[i], barSpace, 250);
+    ctx.fillText(Math.round(times[i]), barSpace, dynamicBarHeight + 225);
+  }
+
+  defineFontStyle(TEXT_FONT, TEXT_COLOR, 'hanging');
+  showText('Ура вы победили!', 130, 25);
+  showText('Список результатов:', 130, 45);
 };
